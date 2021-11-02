d8d84dc26daf2dd037ef5a0b8eca3452f9aed6e0
biko2020 <brahimlion38@hotmail.com>
2021-11-02 22:56:38 +0100

    add responsive menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,43 @@
 <template>
-  <nav>
-    <v-card>
-      <v-app-bar dark fixed dense>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        
-        <v-toolbar-title class="H_logo">
+ 
+<div id="app">
+ <v-toolbar></v-toolbar>
+    <v-app-bar dark fixed>
+      
+    <v-app-bar-nav-icon @click="drawer = true" 
+           class="d-flex d-sm-none" 
+            >
+          </v-app-bar-nav-icon>
+          <v-toolbar-title class="H_logo">
            <v-img src="@/assets/logo/app/logo.png"></v-img>
         </v-toolbar-title>
         <v-spacer></v-spacer>
-
-        <v-btn icon>
+           
+          <v-btn icon >
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
-
+      
+      
         <v-btn icon>
           <v-icon>mdi-dots-vertical</v-icon>
         </v-btn>
-
-        <template v-slot:extension>
-            <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
-          <v-tabs hidden-md-and-up
+      
+        
+         
+          <template  v-slot:extension>
+           <!-- ajouter la class d-none d-sm-flex pour l'afficher un ecran moyenne et plus -->
+         
+          <v-tabs 
+            v-model="tab"
+            center-active
             centered
+            align-with-title
+            class="d-none d-sm-flex justify-center"           
             slider-color="yellow"
             background-color="transparent"
-           
           >
-            <v-tab
+            
+            <v-tab 
               link
               v-for="(menu, index) in menus"
               :key="index"
@@ -33,12 +45,43 @@
               :to="menu.route"
             >
               {{ menu.title }}
-            </v-tab>
-          </v-tabs>
-        </template>
-      </v-app-bar>
-    </v-card>
-  </nav>
+            </v-tab>    
+           </v-tabs>
+       </template>
+    </v-app-bar>
+   <!-- ajouter la barre de navigation -->
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      
+    >
+      <v-list
+        nav
+        dense
+        >
+        <v-list-item-group
+        >
+          <v-list-item 
+            link
+            v-for="(menu, index) in menus" 
+            :key="index"
+            :href="`#tab-${index}`"
+            :to="menu.route"
+          >
+            <v-list-item-title 
+              
+               @click="tab = index">
+              {{ menu.title }}
+             </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- Navigation bar ends -->
+   
+  </div>
+ 
 </template>
 
 
@@ -46,8 +89,12 @@
 export default {
   name: "Header",
 
+
   data() {
     return {
+      drawer:false,
+      tab: null,
+
       menus: [
         { title: "Home", route: "/" },
         { title: "Produits", route: "products" },
@@ -65,14 +112,18 @@ export default {
   position: relative;
   padding: 0;
   top:40%;
-  /*color: #ffca28;
-  font-weight: bold;
-  background-image: url("@/assets/logo/fournisseurs/alfa.png");
-  background-repeat:no-repeat;*/
+
 }
-span.E_logo {
-  color: #bdbdbd;
-  font-weight: bold;
+.item {
+ margin:5px;
+ border-radius: 4px;
 }
+.item :hover {
+  background: green;
+}
+.item  :active {
+background: yellow;
+}
+
 </style>
 
